edadf943f6af0e38cf61221b22db0d6146ab9891
mmulyana <mulyan.t20@gmail.com>
2025-10-20 19:56:18 +0700

    feat: update home controller

--- src/main/java/com/crm/web/controllers/HomeController.java
+++ src/main/java/com/crm/web/controllers/HomeController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  @GetMapping( "/")
-  public String home() {
+  @GetMapping("/")
+  public String indexPage() {
     return "index";
+  }
+
+  @GetMapping("/home")
+  public String homePage() {
+    return "home";
   }
 }
