ac0300965619ded12b04021fae9ae1324fdceb27
mmulyana <mulyan.t20@gmail.com>
2025-10-20 19:53:43 +0700

    feat: user service

---
+++ src/main/java/com/crm/web/services/AppUserService.java
@@ -0,0 +1,27 @@
+package com.crm.web.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.crm.web.models.AppUser;
+import com.crm.web.repositories.AppUserRepository;
+
+public class AppUserService implements UserDetailsService {
+  @Autowired
+  private AppUserRepository repo;
+
+  @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+      AppUser appUser = repo.findByEmail(email);
+
+      if(appUser != null) {
+        var springUser = User.withUsername(appUser.getEmail()).password(appUser.getPassword()).roles(appUser.getRole()).build();
+        return springUser;
+      }
+
+      return null;
+    }
+}
